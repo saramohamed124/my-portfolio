f1af177da16825b5e87274ea9bf9bef4119c507a
saramohamed124 <saramohamaden1234@gmail.com>
2024-03-31 21:47:19 +0200

    cv

--- src/Components/Home/Button.jsx
+++ src/Components/Home/Button.jsx
@@ -1,5 +1,5 @@
 function Button() {
-  const file = "/Sara-Mohamed-Abdullah-CV.pdf";
+  const file = "/my-portfolio/Sara-Mohamed-Abdullah-CV.pdf";
   console.log(file)
   const HandleDownloading = (url) => {
     const fileName = url.split("/").pop();
@@ -34,6 +34,7 @@ function Button() {
           d="M4 15v2a3 3 0 0 0 3 3h10a3 3 0 0 0 3-3v-2m-8 1V4m0 12-4-4m4 4 4-4"
         />
       </svg>
+      up
     </button>
   );
 }
